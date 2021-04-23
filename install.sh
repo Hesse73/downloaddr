805164060f4453c01c7a9583ed609c144239f346
C Hu@ng <1457043769@qq.com>
2021-04-23 12:51:27 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!bin/sh
 
 echo "get packages..."
-sudo pip3 install django, stdiomask, json, sqlite3, hashlib
+sudo pip3 install django
+sudo pip3 install stdiomask
+sudo pip3 install json
+sudo pip3 install sqlite3
+sudo pip3 install hashlib
 echo "create database..."
 
 sudo rm -rf onefile/migrations/
